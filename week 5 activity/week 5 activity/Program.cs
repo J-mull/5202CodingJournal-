92dea1ac4b4cec4c7b2cc07634cf2257e611acdc
James Scadden-Mulligan <3005039@student.toiohomai.ac.nz>
2018-08-20 16:21:47 +1200

    loops

---
+++ week 5 activity/week 5 activity/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_5_activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please type a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please type a second, larger number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("for loop");
+            for(int i = num1; i<=num2; i++)
+            {
+                Console.WriteLine("{0}       {1}       {2}", i, (i*10), (i*100)); 
+
+            }
+
+            int j = num1;
+            Console.WriteLine("while loop");
+            while (j <= num2)
+            {
+                Console.WriteLine("{0}       {1}       {2}", j, j*10,j*100);
+                j++;
+
+             }
+
+            int k = num1;
+            do
+            {
+                Console.WriteLine("{0}       {1}       {2}", j, j * 10, j * 100);
+                j++;
+
+            } while (j <= num2);
+            Console.ReadLine();
+        }
+    }
+}

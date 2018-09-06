9b47a29540055f22ff0020409b8a2ffc14fe11c2
James Scadden-Mulligan <3005039@student.toiohomai.ac.nz>
2018-09-06 14:33:27 +1200

    arrays

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[5];
+
+            Console.Write("Please enter the first name: ");
+            name[0] = Console.ReadLine();
+            Console.Write("Please enter the second name: ");
+            name[1] = Console.ReadLine();
+            Console.Write("Please enter the third name: ");
+            name[2] = Console.ReadLine();
+            Console.Write("Please enter the fourth name: ");
+            name[3] = Console.ReadLine();
+            Console.Write("Please enter the fifth name: ");
+            name[4] = Console.ReadLine();
+
+            Console.WriteLine($"The first name is: {name[0]}");
+            Console.WriteLine($"The last name is: {name[4]}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ harder arrays/harder arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace harder_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            Console.WriteLine("please enter 5 numbers");
+            Console.Write("first number: ");
+            num[0] = int.Parse(Console.ReadLine());
+            Console.Write("second number: ");
+            num[1] = int.Parse(Console.ReadLine());
+            Console.Write("third number: ");
+            num[2] = int.Parse(Console.ReadLine());
+            Console.Write("fourth number: ");
+            num[3] = int.Parse(Console.ReadLine());
+            Console.Write("fifth number: ");
+            num[4] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your five numbers");
+            
+            for (int i = 0; i < num[4] ; i++) 
+            {
+                Console.WriteLine(num[i]);
+
+            }
+            Console.WriteLine("Your five numbers in reverse");
+            for (int i = 4; i >= num[0]; i --)
+            {
+                Console.WriteLine(num[i]);
+
+            }
+            Console.WriteLine(num[0]);
+            Console.ReadLine();
+        }
+    }
+}

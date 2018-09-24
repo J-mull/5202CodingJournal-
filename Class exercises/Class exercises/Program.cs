100f48e64eaa1e6c10e2d59818917f1f6614e005
James Scadden-Mulligan <3005039@student.toiohomai.ac.nz>
2018-09-24 16:51:02 +1200

    classes

---
+++ Class exercises/Class exercises/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the animal type: ");
+            string animalType = Console.ReadLine();
+            Console.Write("Please enter the animal sound: ");
+            string animalSound = Console.ReadLine();
+
+            Animals first = new Animals(animalType, animalSound);
+            Console.WriteLine(first.AnimalSays());
+
+           
+
+
+            Console.ReadLine();
+
+        }
+    }
+    class Animals
+    {
+        // properties 
+        string animalnames;
+        string animalnoises;
+
+        //getters and setters 
+        public string AnimalNames
+        {
+            get { return animalnames; }
+            set { animalnames = value; }
+
+        }
+        public string AnimalNoises
+        {
+            get { return animalnoises; }
+            set { animalnoises = value; }
+        }
+
+        public Animals(string _animalnames, string _animalnoises)
+        {
+            AnimalNames = _animalnames;
+            AnimalNoises = _animalnoises;
+        }
+
+        // public method that returns a string 
+        public string AnimalSays()
+        {
+            return "The " + AnimalNames + " says " + AnimalNoises;
+        }
+
+           
+    }
+}
---
+++ harder arrays 2/harder arrays 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace harder_arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

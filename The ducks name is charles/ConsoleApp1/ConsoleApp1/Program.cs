34137ac608f5ec42a3b5d9b9206ac009f1117167
James Scadden-Mulligan <3005039@student.toiohomai.ac.nz>
2018-09-27 12:38:06 +1200

    last class

---
+++ The ducks name is charles/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an animal type: ");
+            string anName = Console.ReadLine();
+            Console.Write("Please enter the animals name: ");
+            string anType = Console.ReadLine();
+
+            Animal a1 = new Animal(anName, anType);
+            Console.WriteLine(a1.show());
+
+            Console.ReadLine();
+        }
+    }
+    class Animal
+    {
+        public string type;
+        public string name;
+        public int id;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public Animal(string _type, string _name)
+        {
+            Type = _type;
+            Name = _name;
+          
+            
+        }
+
+        public string show()
+        {
+            return $" {Name} is a {Type}";
+        }
+
+    
+    }
+        
+
+
+
+    }
+
